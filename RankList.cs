9ad55b9ddb0dc3634b57948cc0a77b408de2446d
racoooooooon <jlzhang890629@126.com>
2017-05-23 23:28:53 +0800

    array.1

--- RankList.cs
+++ RankList.cs
@@ -7,17 +7,38 @@ public class RankList : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		int score1 = 33;
-		int score2 = 34;
-		int score3 = 35;
-		int score4 = 36;
-		int score5 = 37;
-		int score6 = 38;
-		int score7 = 39;
-		int score8 = 40;
-		int score9 = 31;
+//		int score1 = 33;
+//		int score2 = 34;
+//		int score3 = 35;
+//		int score4 = 36;
+//		int score5 = 37;
+//		int score6 = 38;
+//		int score7 = 39;
+//		int score8 = 40;
+//		int score9 = 31;
+//
+//		guiText.text = "Rank list\n 1." + score1 + "\n2." + score2;
 
-		guiText.text = "Rank list\n 1." + score1 + "\n2." + score2;
+		//1 method init array
+//		int[] scores = new int[10];
+//		scores [0] = 22;
+//		scores [1] = 34;
+//		scores [2] = 3;
+//		......
+//		//2 method init array
+//		int[] scores = new int[10]{33,3,45,56,76,87,345,556,45};
+
+		int[] scores = {33,3,45,56,76,87,345,556,45};
+		//print (scores[8]);
+		string str = "Rank List\n\n";
+
+		for(int i=0;i<=8;i++){
+			str = str + (i+1) + ":"+scores[i]+"\n";
+
+		
+		}
+
+		guiText.text = str;
 	}
 	
 	// Update is called once per frame
